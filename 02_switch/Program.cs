68115c982d39baccce67c3009f8389574eb172b8
David-Meijer <67479247+David-Meijer@users.noreply.github.com>
2025-05-04 18:13:18 +0200

    Switch (#2)
    * switch project added
    
    * Switch statement done
    
    * mini project added
    
    * folder structure change
    
    * names changed
    
    * mini project finished
    
    * refactored finished project
    
    * project named differently
    
    * name change
    
    * rename csproj

---
+++ 02_switch/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+string firstName = "David";
+int age = 29;
+
+switch (firstName.ToLower())
+{
+    case "david":
+        Console.WriteLine("Hey you!");
+        break;
+    
+    case "caesar" or "cleopatra":
+        Console.WriteLine("You are very old!");
+        break;
+    
+    default:
+        Console.WriteLine("I don't think i know you..");
+        break;
+}
+
+switch (age)
+{
+    case >= 0 and < 4:
+        Console.WriteLine("Your are not in school yet.");
+        break;
+    
+    case >= 4 and < 17:
+        Console.WriteLine("You are most likely already in school.");
+        break;
+    
+    case >= 17 and < 26:
+        Console.WriteLine("Do you already have a job or still studying?");
+        break;
+
+    case >= 26 and <= 67:
+        Console.WriteLine("You are probably working!");
+        break;
+
+    case > 67 and <= 120:
+        Console.WriteLine("You probably stopped working already!");
+        break;
+
+    default:
+        Console.WriteLine("I wan't expecting to encounter an age of " + age);
+        break;
+}
---
+++ 03_mini_project_1_greet_student_or_professor/Program.cs
@@ -0,0 +1,40 @@
+﻿
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your age? ");
+string ageText = Console.ReadLine(); //Readline only gives back string values
+
+bool isValidAge = int.TryParse(ageText, out int age);
+
+if (!isValidAge)
+{
+    Console.WriteLine("You did not supply a valid age!");
+    Console.WriteLine("Exiting program...");
+    return; //exit the program
+}
+
+//Determine correct formatting of name based on proffer names and age
+string firstNameLowerCase = firstName.ToLower();
+string formattedName;
+if ((firstNameLowerCase == "bob" || firstNameLowerCase == "sue") && age > 21)
+{
+     formattedName = $"professor {firstName}";
+}
+else
+{
+    formattedName = firstName;
+}
+
+//Great student or professor
+if (age >= 21) 
+{
+    Console.WriteLine($"Hello {formattedName}");
+}
+//Somebody whose age is probably too low to be a student or professor
+else
+{   
+    int yearsToWait = 21 - age;
+    Console.WriteLine($"Hello {formattedName}, you should probably wait for {yearsToWait} more years to start studying!");
+}
+
