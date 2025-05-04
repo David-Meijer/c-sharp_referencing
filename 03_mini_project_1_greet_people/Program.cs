afe53b3962cd0a3f605c843c88cfe6ed1265350d
David Meijer <David.meijer95@hotmail.com>
2025-05-04 12:53:12 +0200

    mini project finished

--- 03_mini_project_1_greet_people/Program.cs
+++ 03_mini_project_1_greet_people/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your age? ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+string firstNameLowerCase = firstName.ToLower();
+
+if (firstNameLowerCase == "bob" || firstNameLowerCase == "Sue") 
+{
+    Console.WriteLine("Hello Professor!");
+}
+else if ((firstNameLowerCase != "bob" || firstNameLowerCase != "Sue") && age >= 21)
+{
+    Console.WriteLine("Hello student!")
+} 
+else
+{
+    Console.WriteLine("You should probably wait some more time to start studying!");
+}
+
