53246e8d7f8ffa83b5906eedca1fd49e8dba9ac6
David-Meijer <67479247+David-Meijer@users.noreply.github.com>
2025-05-05 16:25:02 +0200

    Arrays (#4)
    * new branch
    
    * array script added

---
+++ 05_arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//specify array length and indivually assign
+string[] namesArray = new string[2];
+namesArray[0] = "David";
+namesArray[1] = "Bob";
+
+Console.WriteLine($"Hello {namesArray[0]}");
+Console.WriteLine($"Hello {namesArray[1]}");
+Console.WriteLine($"We just greeted {namesArray.Length} people!"); //access the array length
+
+//specify array by assigning the array to it
+string[] anotherNamesArray = ["Foo", "Bar"];
+
+Console.WriteLine($"{anotherNamesArray[0]} {anotherNamesArray[1]}!");
+
+//specify array by using a splitted string
+string historicalFigures = "Cleopatra,Caesar,Einstein,Columbus";
+string[] historicalFiguresArray = historicalFigures.Split(',');
+
+Console.WriteLine($"Hello {historicalFiguresArray[3]}");
+
+//specifying array by giving it a sequence (note that the array length is not specified @ new string[])
+string[] favouriteFoodArray = new string[] {"Carbonara", "Pizza", "Curry"};
+Console.WriteLine($"Lets eat {favouriteFoodArray[0]} tonight!");
