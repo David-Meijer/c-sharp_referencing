a7105ba7da967e83c2165b7c83093f8b97fa535f
David Meijer <David.meijer95@hotmail.com>
2025-05-05 14:34:20 +0200

    Do while loop implemented

--- 04_(do)_while/Program.cs
+++ 04_(do)_while/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//Do-while loop
+//Run code at least once
+bool isValidAge;
+int age;
+
+do
+{
+    Console.Write("Please enter you age: ");
+    isValidAge = int.TryParse(Console.ReadLine(), out age);
+    if  (!isValidAge || age > 125 || age < 0)
+    {   
+        isValidAge = false;
+        Console.WriteLine("You did not enter a valid age, please try again");
+    }
+}
+while (!isValidAge);
+
+Console.WriteLine("Your entered age was validated to be correct!");
+
+
+//While loop
+//Run code 0 or more times
+bool isValidYear = false;
+int currentYear;
+
+while (!isValidYear)
+{
+    Console.Write("Please enter the current year: ");
+    isValidYear = int.TryParse(Console.ReadLine(), out currentYear);
+    if (!isValidYear || currentYear < 2025)
+    {
+        isValidYear = false;
+        Console.WriteLine("You did not enter a valid year, please try again");
+    }
+}
+
+Console.WriteLine("Your entered year was validated to be correct!");
